050c304bd382ba380d113b9973cf0083fa1ffd39
Uzair Khan <38373166+uzair-balouch@users.noreply.github.com>
2019-02-25 02:27:48 -0800

    Update FriendNumber.py

--- FriendNumber.py
+++ FriendNumber.py
@@ -1,1 +1,23 @@
 
+
+
+
+import MyNumber
+
+
+def MFine():
+    print('I am Fine Jimmy! How\'s your day going so far.?')
+
+def SameHere():
+    print('Same here bro..I am also too bored today.. \' -\'')
+
+def YeahSure():
+    print('Yeah Sure Jimmy why not,,')
+
+def OkIWillChoose():
+    return 'Ok Jimmy I will choose'
+
+def AgainSecondNumber():
+    return 'Ok My second number..hmmm will be this..  :: '
+def Ok_Makes_Sense():
+    return 'Ok that makes sense!..Go ahead'
